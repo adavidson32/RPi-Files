9968fb1ef76621587e1fdd7eaf3669b89cb6bca2
adavidson32 <alex@davidson1.com>
2017-04-09 20:15:41 -0400

    Update OutletBox_MQTT.py

--- python/code/OutletBox_MQTT.py
+++ python/code/OutletBox_MQTT.py
@@ -13,6 +13,20 @@ def connected(client):
 def disconnected(client):
     print('Disconnected from Adafruit IO')
     sys.exit(1)
-   
+    
 def message(client, feed_id, payload):
-  
+    print('Feed {0} received new value {1}'.format(feed_id, payload)
+    if payload=='outlet 1 on':
+          print('Outlet-1 Turned ON via IFTTT')
+    elif payload=='outlet 1 off':
+          print('Outlet-1 Turned OFF via IFTT')
+          
+client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
+          
+client.on_connect    = connected
+client.on_disconnect = disconnected
+client.on_message    = message
+          
+client.connect()
+          
+client.loop_blocking()
