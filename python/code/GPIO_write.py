a7861fd0f109583275e227a722bcdef90cc910f7
adavidson32 <alex@davidson1.com>
2017-04-09 23:18:30 -0400

    Update GPIO_write.py

--- python/code/GPIO_write.py
+++ python/code/GPIO_write.py
@@ -2,20 +2,19 @@ import RPi.GPIO as GPIO
 import sys
 
 GPIO.setmode(GPIO.BCM)
-if not(len(sys.argv) == 3):
-  print("len(argv)!=3... Please manually enter GPIO_pin and GPIO_function")
-  GPIO_pin = int(input("Which pin to control? : "))
-  GPIO_function = input("Function to apply? : ")
-else:
+if len(sys.argv) == 3:
   GPIO_pin = int(sys.argv[1])
   GPIO_function = sys.argv[2]
-
-print("GPIO_pin : ", GPIO_pin)
-print("GPIO_function : ", GPIO_function)
+else:
+  print("len(argv)!=3... Please manually enter GPIO_pin and GPIO_function")
+  GPIO_pin = int(input("Which pin to control? : "))
+  GPIO_function = input("Function to apply? : ")  
 
 GPIO.setup(GPIO_pin, GPIO.OUT)
 
 if (GPIO_function == "HIGH"):
   GPIO.output(GPIO_pin, GPIO.HIGH)
+  print("GPIO#{0} set to HIGH...".format(GPIO_pin))
 elif (GPIO_function == "LOW"):
   GPIO.output(GPIO_pin, GPIO.LOW)
+  print("GPIO#{0} set to LOW...".format(GPIO_pin))
