812f9cad692280e5e090308b11029bc908e4fd49
adavidson32 <alex@davidson1.com>
2017-04-09 20:10:01 -0400

    Update OutletBox_MQTT.py

--- python/code/OutletBox_MQTT.py
+++ python/code/OutletBox_MQTT.py
@@ -1,1 +1,18 @@
+import sys
+from Adafruit_IO import MQTTClient
 
+ADAFRUIT_IO_KEY      = ' '
+ADAFRUIT_IO_USERNAME = 'adavidson93'
+
+FEED_ID = 'IFTTT'
+
+def connected(client):
+    print('Connected to Adafruit IO. Listening for {0} changes...'.format(FEED_ID))
+    client.subscribe(FEED_ID)
+
+def disconnected(client):
+    print('Disconnected from Adafruit IO')
+    sys.exit(1)
+   
+def message(client, feed_id, payload):
+  
