12c973e8fac21aafa7e887b7fab72322f25d625e
Alex Davidson <alex@davidson1.com>
2017-04-28 02:00:49 +0400

    from pi-3 added __init__ and .on to relays.py

--- outlet_box/io/relays.py
+++ outlet_box/io/relays.py
@@ -1,1 +1,22 @@
+import RPi-GPIO as io
+from time import sleep, time
+
+class relays:
+    def __init__(self, pin, active_low=True):
+	io.setmode(io.BCM)
+	io.setwarnings(
+	self.pin = pin
+	self.active_low = active_low
+	io.setup(self.pin, io.OUT)
+	if active_low:
+	    io.output(self.pin, io.HIGH)
+	elif not(active_low):
+	    io.output(self.pin, io.LOW)
+
+    def on(self):
+	if self.active_low:
+	    io.output(self.pin, io.LOW)
+	else:
+	    io.output(self.pin, io.HIGH)
+
 
