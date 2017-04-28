66230766a9d460502abe84ea93484db55e54274a
adavidson32 <alex@davidson1.com>
2017-04-27 20:37:06 -0400

    Update single_relay.py

--- outlet_box/single_relay.py
+++ outlet_box/single_relay.py
@@ -2,26 +2,14 @@ import RPi-GPIO as io
 from time import sleep, time
 
 class relays:
-    def __init__(self, pin, active_low=True):
+    def __init__(self, pin):
 	io.setmode(io.BCM)
-	io.setwarnings(
+	io.setwarnings(False)
 	self.pin = pin
-	self.active_low = active_low
-	io.setup(self.pin, io.OUT)
-	if active_low:
-	    io.output(self.pin, io.HIGH)
-	elif not(active_low):
-	    io.output(self.pin, io.LOW)
+	io.setup(self.pin, io.OUT, initial=HIGH)
 
     def on(self):
-	if self.active_low:
-	    io.output(self.pin, io.LOW)
-	else:
-	    io.output(self.pin, io.HIGH)
-
-
+	io.output(self.pin, io.LOW)
+		
     def off(self):
-	if self.active_low:
-	    io.output(self.pin, io.HIGH)
-	else:
-	    io.output(self.pin, io.LOW)
+	io.output(self.pin, io.HIGH)
