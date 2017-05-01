627b69a287b95accc934b3c4b037211bbd9f2c44
Alex Davidson <alex@davidson1.com>
2017-05-01 00:26:24 -0400

    from desktop

--- outlet_box/0430/type_test.py
+++ outlet_box/0430/type_test.py
@@ -1,12 +1,18 @@
 # type_test.py
 
 def type_test(variable):
-    ret = ('int', variable) if is_int(variable)
-    ret = ('str', len(variable) if is_str(variable)
-    ret = ('tuple', len(variable) if is_tuple(variable)
-    ret = ('list', len(variable) if is_list(variable)
-    ret = ('dict', len(variable) if is_dict(variable)
-    ret = ('str-int', is_string(variable) if is_strint(variable) >= 0
+    if is_int(variable):
+        ret = ('int', variable)
+    elif is_str(variable):
+        ret = ('str', len(variable)
+    elif is_tuple(variable):
+        ret = ('tuple', len(variable)
+    elif is_list(variable):
+        ret = ('list', len(variable)
+    elif is_dict(variable):
+        ret = ('dict', len(variable)
+    elif is_strint(variable):
+        ret = ('str-int', int(variable)
     return ret
 
 def is_int(variable):
@@ -15,16 +21,16 @@ def is_int(variable):
 def is_str(variable):
     return True if (type(variable) == type('str')) else False
 
-# If variable != str return -2, if int(variable) causes error return -1, else return int(variable)
 def is_strint(variable):
     str_test = True if (type(variable) == type('str')) else False
-    if not(str_test):
-        return -2
-    else:
+    if str_test:
         try:
-            return int(variable)
+            num = int(variable)
+            return True
         except:
-            return -1
+            return False
+    else:
+        return False
 
 def is_tuple(variable):
     return True if (type(variable) == type(('',''))) else False
