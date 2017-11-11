4de42e3f3a44969b427cb8770d676bbfc5f910ff
adavidson32 <alex@davidson1.com>
2017-11-11 14:09:43 -0500

    Update 4x_Relay_Test.py

--- python/code/4x_Relay_Test.py
+++ python/code/4x_Relay_Test.py
@@ -1,23 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(23,GPIO.OUT)
-GPIO.setup(24,GPIO.OUT)
-GPIO.setup(25,GPIO.OUT)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO.OUT,True)
+GPIO.setup(23,GPIO.OUT,True)
+GPIO.setup(24,GPIO.OUT,True)
+GPIO.setup(25,GPIO.OUT,True)
 
 while True:
-  GPIO.output(18, True)
-  time.sleep(0.5)
   GPIO.output(18, False)
-  GPIO.output(23, True)
   time.sleep(0.5)
+  GPIO.output(18, True)
   GPIO.output(23, False)
-  GPIO.output(24, True)
   time.sleep(0.5)
+  GPIO.output(23, True)
   GPIO.output(24, False)
-  GPIO.output(25, True)
   time.sleep(0.5)
+  GPIO.output(24, True)
   GPIO.output(25, False)
+  time.sleep(0.5)
+  GPIO.output(25, True)
 GPIO.cleanup()
