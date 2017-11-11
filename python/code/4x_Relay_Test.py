fc9d22ffa7c46aa4ec1f2f1a1ac67b5b18363251
adavidson32 <alex@davidson1.com>
2017-11-11 14:10:42 -0500

    Update 4x_Relay_Test.py

--- python/code/4x_Relay_Test.py
+++ python/code/4x_Relay_Test.py
@@ -2,10 +2,14 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18,GPIO.OUT,True)
-GPIO.setup(23,GPIO.OUT,True)
-GPIO.setup(24,GPIO.OUT,True)
-GPIO.setup(25,GPIO.OUT,True)
+GPIO.setup(18,GPIO.OUT)
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(25,GPIO.OUT)
+GPIO.output(18, True)
+GPIO.output(23, True)
+GPIO.output(24, True)
+GPIO.output(25, True)
 
 while True:
   GPIO.output(18, False)
